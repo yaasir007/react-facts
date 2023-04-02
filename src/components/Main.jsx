27097c7b3ab6b777207faac43eed7dce8c4dc791
Yaasir Cheekoory <yaasir1997@gmail.com>
2023-04-03 03:05:33 +0400

    css completed on main

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -5,7 +5,16 @@ const Main = () => {
     <section className="main">
       <div className="main-container">
         <div className="main-wrapper">
-          
+
+          <div className="main__title">Fun facts about React</div>
+
+          <ul className="main__facts">
+            <li className="main__fact">Was first released in 2013</li>
+            <li className="main__fact">Was originally created by Jordan Walke</li>
+            <li className="main__fact">Has well over 100K stars on GitHub</li>
+            <li className="main__fact">Is maintained by Facebook</li>
+            <li className="main__fact">Powers thousands of enterprise apps, including mobile apps</li>
+          </ul>
         </div>
       </div>
     </section>
